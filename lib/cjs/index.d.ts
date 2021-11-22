cd9dce100d6bfad18efa8485f1cb3e38b142310e
Ceamatu Cristian <cristian.ceamatu@assist.ro>
2021-11-22 17:58:32 +0200

    Initial Commit

---
+++ lib/cjs/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+declare const SayHello: ({ name }: {
+    name: string;
+}) => JSX.Element;
+export default SayHello;
+export * from './Components/Button/Button';
---
+++ lib/esm/Components/Button/Button.d.ts
@@ -0,0 +1,14 @@
+import * as React from 'react';
+export interface Props {
+    /** Button content  */
+    children: React.ReactNode;
+    /** Callback to handle the click event  */
+    onClick: () => void;
+    /**
+     * Disables onClick
+     *
+     * @default false
+     **/
+    disabled?: boolean;
+}
+export declare const Button: (props: Props) => JSX.Element;
